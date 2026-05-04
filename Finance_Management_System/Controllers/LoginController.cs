27ba832872c184279a54029f062036843a04d783
amantesfa1010 <amantesfa1010@gmail.com>
2026-05-04 15:48:13 +0300

    Add project files.

---
+++ Finance_Management_System/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finance_Management_System.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Finance_Management_System/Models/LoginUI.cs
@@ -0,0 +1,9 @@
+﻿namespace Finance_Management_System.Models
+{
+    public class LoginUI
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
